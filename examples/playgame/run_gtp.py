d104dc93669afed59d73c0c469182836a426bf6b
mecha2k <mecha2k@naver.com>
2020-11-21 06:04:30 +0900

    Merge branch 'master' of https://github.com/mecha2k/mygo
    # Conflicts:
    #	examples/end_to_end.py
    #	examples/webapp/web_demo.py

--- examples/playgame/run_gtp.py
+++ examples/playgame/run_gtp.py
@@ -4,7 +4,7 @@ from dlgo.agent.predict import load_prediction_agent
 from dlgo.agent import termination
 import h5py
 
-model_file = h5py.File("../agents/deep_bot_h5py.h5", "r")
+model_file = h5py.File("../../agents/deep_bot_h5py.h5", "r")
 agent = load_prediction_agent(model_file)
 strategy = termination.get("opponent_passes")
 termination_agent = termination.TerminationAgent(agent, strategy)
